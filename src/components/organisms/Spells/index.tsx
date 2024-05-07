73e79bf22e26aac4ff5ac6f41981cdac6e2f7f7c
sudipstha08 <sudipstha08@gmail.com>
2024-05-07 10:39:02 +0545

    feat: add button for show more

--- src/components/organisms/Spells/index.tsx
+++ src/components/organisms/Spells/index.tsx
@@ -1,14 +1,25 @@
 import { FC, useCallback, useEffect, useState } from 'react'
 import { useSnapshot } from 'valtio'
 import { Modal, Result, Spin } from 'antd'
+import _ from 'lodash'
+import { fadeIn } from 'react-animations'
+import Radium, { StyleRoot } from 'radium'
 import { SpellCard, SpellDetails } from '../../molecules'
 import { useSpells } from '../../../queries'
 import { spellStore } from '../../../store/spellStore'
+
+const styles = {
+  bounce: {
+    animation: 'x 3s',
+    animationName: Radium.keyframes(fadeIn, 'bounce'),
+  },
+}
 
 const Spells: FC = () => {
   const { data: spellsData, error, isLoading } = useSpells({ enabled: true })
   const { currentSpell } = useSnapshot(spellStore)
   const [isModalOpen, setIsModalOpen] = useState(false)
+  const [spellCount, setSpellsCount] = useState(20)
 
   useEffect(() => {
     if (currentSpell) {
@@ -20,6 +31,12 @@ const Spells: FC = () => {
     setIsModalOpen(prevState => !prevState)
     spellStore.setCurrentSpell(null)
   }, [currentSpell])
+
+  const onCountClick = () => {
+    setSpellsCount(prevCount =>
+      prevCount === 20 ? (spellsData?.spells?.length as number) : 20,
+    )
+  }
 
   if (isLoading) {
     return (
@@ -46,24 +63,37 @@ const Spells: FC = () => {
       <section className="bg-no-repeat dark:bg-[#0B1120] min-h-[87.4vh] pb-10 pr-10 pl-10">
         <h3 className="text-6xl text-white text-center pt-10 mb-12">Spells</h3>
         <div className="m-auto max-w-[1480px]">
-          <div className="grid grid-cols-1 gap-3 lg:gap-5 sm:grid-cols-2 lg:grid-cols-3">
-            {spellsData?.spells?.map(spell => {
-              return (
-                <SpellCard
-                  key={spell.id}
-                  id={spell.id}
-                  name={spell.name}
-                  desc={spell.desc}
-                  level={spell.level}
-                />
-              )
-            })}
-          </div>
+          <StyleRoot>
+            <div
+              className="grid grid-cols-1 gap-3 lg:gap-5 sm:grid-cols-2 lg:grid-cols-3"
+              style={styles.bounce}
+            >
+              {spellsData?.spells?.slice(0, spellCount).map(spell => {
+                return (
+                  <SpellCard
+                    key={spell.id}
+                    id={spell.id}
+                    name={spell.name}
+                    desc={spell.desc}
+                    level={spell.level}
+                  />
+                )
+              })}
+            </div>
+          </StyleRoot>
+        </div>
+        <div className="mt-5 text-center">
+          <button
+            className="dark:bg-slate-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={onCountClick}
+          >
+            {spellCount === 20 ? 'Show More' : 'Show Less'}
+          </button>
         </div>
       </section>
 
       <Modal
-        title={currentSpell}
+        title={_(_.capitalize(currentSpell)).replace('-', ' ')}
         open={isModalOpen}
         onOk={closeModal}
         onCancel={closeModal}
