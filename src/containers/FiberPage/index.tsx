296795b6eeee42f3369693cffbe84d27b075824c
sudipstha08 <sudipstha08@gmail.com>
2024-05-16 21:56:42 +0545

    react fiber init

--- src/containers/FiberPage/index.tsx
+++ src/containers/FiberPage/index.tsx
@@ -1,15 +1,43 @@
-import React from 'react'
-// import { Canvas } from '@react-three/fiber'
+import { FC } from 'react'
+import { Canvas, Color, Vector3 } from '@react-three/fiber'
+import styled from 'styled-components'
 
-const FiberPage = () => {
+const Container = styled.div`
+  margin-top: 100px;
+`
+
+const Cube: FC<{ position: Vector3; size: any; color: Color }> = ({
+  position,
+  size,
+  color,
+}) => {
   return (
-    <h1>hhhh</h1>
-    // <Canvas>
-    //   <mesh>
-    //     <boxGeometry />
-    //   </mesh>
-    // </Canvas>
+    <mesh position={position}>
+      <boxGeometry args={size} />
+      <meshStandardMaterial color={color} />
+    </mesh>
+  )
+}
+
+const FiberPage: FC = () => {
+  return (
+    <Container>
+      <Canvas>
+        <directionalLight position={[0, 0, 2]} intensity={0.5} />
+        <ambientLight intensity={0.1} />
+
+        <group position={[0, -1, 0]}>
+          <Cube position={[-4, -2, 0]} size={[3, 3, 1]} color="red" />
+
+          <Cube position={[3, -2, 0]} size={[3, 3, 1]} color="orange" />
+          <Cube position={[-4, 2, 0]} size={[3, 3, 1]} color="blue" />
+          <Cube position={[2, 3, 0]} size={[3, 4, 1]} color="yellow" />
+        </group>
+      </Canvas>
+    </Container>
   )
 }
 
 export { FiberPage }
+
+// mesh - represents polygon objects
