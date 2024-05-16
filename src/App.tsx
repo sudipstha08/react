c81348e4a0daea687b6a0cc8ce8f63ad44657bea
sudipstha08 <sudipstha08@gmail.com>
2024-05-16 19:07:39 +0545

    feat: add react fiber

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { Router, Switch, Route } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { HomePage, PortfolioPage, SpellsPage, UploadPage } from './containers'
+import {
+  FiberPage,
+  HomePage,
+  PortfolioPage,
+  SpellsPage,
+  UploadPage,
+} from './containers'
 import './App.css'
 
 const queryClient = new QueryClient()
@@ -17,6 +23,7 @@ function App() {
             <Route exact path="/upload" component={UploadPage} />
             <Route exact path="/spells" component={SpellsPage} />
             <Route exact path="/portfolio" component={PortfolioPage} />
+            <Route exact path="/fiber" component={FiberPage} />
           </Switch>
         </Router>
       </QueryClientProvider>
---
+++ src/containers/FiberPage/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import { Canvas } from '@react-three/fiber'
+
+const FiberPage = () => {
+  return (
+    <h1>hhhh</h1>
+    // <Canvas>
+    //   <mesh>
+    //     <boxGeometry />
+    //   </mesh>
+    // </Canvas>
+  )
+}
+
+export { FiberPage }
